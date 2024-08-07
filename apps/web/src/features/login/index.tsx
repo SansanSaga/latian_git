2b07283db528f200d64bcee5b98f94cb9222e59e
SansanSaga <154500077+SansanSaga@users.noreply.github.com>
2024-08-07 11:40:07 +0700

    feat: login (#2)

--- apps/web/src/features/login/index.tsx
+++ apps/web/src/features/login/index.tsx
@@ -1,49 +1,36 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useFormik } from "formik";
-import { RegisterSchema } from "./schemas/RegisterSchema";
-import useRegister from "@/hooks/api/auth/useRegister";
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useFormik } from 'formik'
+import React from 'react'
+import { LoginSchema } from './schemas/LoginSchema'
+import useLogin from '@/hooks/api/auth/useLogin'
 
-const RegisterPage = () => {
-  const { register, isLoading } = useRegister();
+const LoginPage = () => {
+  const {login, isLoading} = useLogin();
 
   const formik = useFormik({
     initialValues: {
-      name: "",
       email: "",
       password: "",
     },
-    validationSchema: RegisterSchema,
+    validationSchema: LoginSchema,
     onSubmit: async(values) => {
-      await register(values);
+      await login(values);
     },
   });
-
-  console.log(formik.errors);  
-
   return (
     <main className="flex justify-center pt-20">
       <Card className="w-[350px]">
       <CardHeader>
-        <CardTitle>Sign Up</CardTitle>
+        <CardTitle>Sign In</CardTitle>
       </CardHeader>
       <CardContent>
         <form onSubmit={formik.handleSubmit}>
           <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input name="name" type="text" placeholder="Your Name" value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
-              {!!formik.touched.name && !!formik.errors.name ? (<p className="text-xs text-red-500">{formik.errors.name}</p>) : null}
-            </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="email">Email</Label>
               <Input name="email" type="email" placeholder="Your Email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
@@ -61,6 +48,6 @@ const RegisterPage = () => {
     </Card>
     </main>
   )
-};
+}
 
-export default RegisterPage;
+export default LoginPage
