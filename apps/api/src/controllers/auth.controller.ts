bca1be654ce7b86f3cac77b138a616419a5e7b72
SansanSaga <154500077+SansanSaga@users.noreply.github.com>
2024-08-08 11:35:47 +0700

    feat: forgot password (#4)

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+import { forgotPasswordService } from '@/services/auth/forgotPassword.service';
 import { loginService } from '@/services/auth/login.service';
 import { registerService } from '@/services/auth/register.service';
 import { NextFunction, Request, Response } from 'express';
@@ -15,6 +16,15 @@ export class AuthController {
   async login(req: Request, res: Response, next: NextFunction) {
     try {
       const result = await loginService(req.body);
+      return res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async forgotPassword(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await forgotPasswordService(req.body.email);
       return res.status(200).send(result);
     } catch (error) {
       next(error);
